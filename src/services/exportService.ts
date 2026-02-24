d17b0b44b5db93422b89d26e0e419ad9ccb57dde
Anh Nguyen Hoang <anhnh@tech.est-rouge.com>
2026-02-24 16:18:38 +0700

    fix: Use expo-file-system/legacy to avoid deprecation error
    Changed from:
      import * as FileSystem from 'expo-file-system'
    
    To:
      import * as FileSystem from 'expo-file-system/legacy'
    
    This fixes the writeAsStringAsync deprecation error.
    The legacy API works perfectly and is still supported.

--- src/services/exportService.ts
+++ src/services/exportService.ts
@@ -1,4 +1,4 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import * as Sharing from 'expo-sharing';
 import { transactionService } from './transactionService';
 import { categoryService } from './categoryService';
